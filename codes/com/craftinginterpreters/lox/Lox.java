db069c06bf1d90b91874fe28b63d1e5e8c10d7ba
cito23057 <cito23057@g.nihon-u.ac.jp>
2025-12-02 10:08:56 +0900

    update scanToken and delete java.util.Scanner

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -8,7 +8,8 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
+//ここが競合？
+//import java.util.Scanner;
 
 public class Lox {
     static boolean hadError = false;
